2d3a766b449a112ab975c332dd009e2f4c97a300
Willy Rincon H <willyrinconh@gmail.com>
2023-05-27 09:54:24 -0500

    modificaciones

--- app.js
+++ app.js
@@ -4,3 +4,6 @@ console.log('Linea de codigo1');
 console.log('Linea de codigo2   ');
 
 console.log('nueva linea 3')
+
+console.log('nuevo cambio para ver con git diff')
+console.log('otra linea con nuevo cambio para ver con git diff')
--- inicio.js
+++ inicio.js
@@ -1,1 +1,5 @@
 console.log("he modificado el inicio");
+
+
+console.log('nuevo cambio en inicio para ver con git diff')
+console.log('otra linea con nuevo cambio een inicio para ver con git diff')
